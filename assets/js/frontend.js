1fbab7ca15d9e128ecf94bf07dc7f478d0894b65
Alexander Sh <alexander@probegin.com>
2023-09-30 21:00:59 +0300

    dashboard sceleton imlementstion

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -12,11 +12,27 @@ $(window).on('scroll', function () {
     }
 });
 $(function () {
+
     $('a[data-toggle="accordion-content"]').on('click', function (e) {
         e.preventDefault();
+
         $(this).toggleClass('active');
         $(this).next().slideToggle('slow');
         $('.accordion-content').not($(this).next()).slideUp('slow');
         $('.accordion-title').not($(this)).removeClass('active');
+    });
+
+    $('a[aria-controls="blog"]').on('click', function (e) {
+        e.preventDefault();
+
+        let arrow = $(this).children('.arrow');
+
+        if (arrow.hasClass('bi-arrow-down-short')) {            
+            arrow.removeClass('bi-arrow-down-short').addClass('bi-arrow-right-short');
+        } else {
+            arrow.removeClass('bi-arrow-right-short').addClass('bi-arrow-down-short');
+        }
+        
+        
     });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,10 @@ Encore
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-     */
+     */    
     .addEntry('app', './assets/app.js')
     .addEntry('frontend', './assets/js/frontend.js')
+    .addStyleEntry('sidebar', './assets/styles/sidebar.css')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
