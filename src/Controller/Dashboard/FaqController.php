78532929401687fbabad338b0ff75c78a2249c10
Alexander Sh <alexander@probegin.com>
2023-10-01 21:15:41 +0300

    fix

--- src/Controller/Dashboard/FaqController.php
+++ src/Controller/Dashboard/FaqController.php
@@ -6,13 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/dashboard/faq')]
 class FaqController extends AbstractController
 {
-    #[Route('/dashboard/faq', name: 'app_dashboard_faq')]
+
+    #[Route('/', name: 'app_dashboard_faq')]
     public function index(): Response
     {
         return $this->render('dashboard/content/faq/index.html.twig', [
-            'data' => 'Questions content',
+                    'data' => 'Questions content',
+        ]);
+    }
+    
+    #[Route('/create', name: 'app_dashboard_faq_create')]
+    public function create(): Response
+    {
+        return $this->render('dashboard/content/faq/index.html.twig', [
+                    'data' => 'Questions content',
         ]);
     }
 }
