f59af061bd81cbc3956e40bbee71c6bf5e41587f
Alexander Sh <alexander@probegin.com>
2023-10-11 05:55:59 +0300

    install google mailer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\Type\ContactType;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mailer\MailerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use App\Entity\Contact;
-use App\Form\Type\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 
 class ContactController extends AbstractController
 {
@@ -26,6 +30,7 @@ class ContactController extends AbstractController
             Request $request,
             EntityManagerInterface $em,
             ValidatorInterface $validator,
+            MailerInterface $mailer,
     ): Response
     {
         $contact = new Contact();
@@ -41,6 +46,23 @@ class ContactController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $em->persist($contact);
             $em->flush();
+
+//            $email = (new Email())
+//                    ->from(new Address($form->get('email')->getData(), $form->get('name')->getData()))
+//                    ->to(new Address('alexandershtyher@gmail.com', 'Alexander Sh'))
+//                    //->cc('cc@example.com')
+//                    //->bcc('bcc@example.com')
+//                    ->replyTo(new Address('julyshtyher@gmail.com', 'July Sh'))
+//                    //->priority(Email::PRIORITY_HIGH)
+//                    ->subject($form->get('subject')->getData())
+//                    ->text('Sending emails is fun again!')
+//                    ->html($form->get('message')->getData());
+//
+//            try {
+//                $mailer->send($email);
+//            } catch (TransportExceptionInterface $e) {
+//                throw new \Exception($e->getMessage());
+//            }
 
             return $this->redirectToRoute('contact_success');
         }
