4d843dfdf91fe0b0d27102eafc6141165a0a9281
Alexander Sh <alexander@probegin.com>
2023-10-26 07:12:08 +0300

    cleanup expires token

---
+++ src/Command/RequestTokenCleanupCommand.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Command;
+
+use App\Repository\ResetPasswordRequestRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand('app:requesttoken:cleanup', 'Deletes request tokens from database')]
+class RequestTokenCleanupCommand extends Command
+{
+
+    const PERFORM = 'perform';
+
+    /**
+     * 
+     * @param ResetPasswordRequestRepository $repository
+     */
+    public function __construct(
+            private ResetPasswordRequestRepository $repository,
+    )
+    {
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->addOption(self::PERFORM, null, InputOption::VALUE_NONE, 'Execute');
+    }
+
+    /**
+     * 
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(
+            InputInterface $input,
+            OutputInterface $output,
+    ): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $now = new \DateTime();
+
+        $count = 0;
+        $execute = true;
+
+        if ($input->getOption(self::PERFORM)) {
+            $io->note('performing...');
+            $result = $this->repository
+                    ->createQueryBuilder('rt')
+                    ->delete()
+                    ->where('rt.expiresAt < :expiresAt')
+                    ->setParameter('expiresAt', $now->format('Y-m-d H:i'));
+
+            $count = $result->getQuery()->getSingleScalarResult();
+        } else {
+            $execute = false;
+        }
+
+        if (!$execute) {
+            $io->warning(sprintf('Invalid optitons. Use [--%s] option', self::PERFORM));
+            return Command::INVALID;
+        } else {
+            $io->success(sprintf('Deleted "%d" old request tokens.', $count));
+        }
+
+        return Command::SUCCESS;
+    }
+}
