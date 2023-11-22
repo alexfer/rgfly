b400fd6caf9ce1168d80cfa80087a4812f2ca76b
Alexander Sh <alexander@probegin.com>
2023-11-22 20:03:34 +0200

    improve view blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,28 +2,57 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\EntryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\{Request, Response};
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/blog')]
 class BlogController extends AbstractController
 {
+    /**
+     * @param Request $request
+     * @param EntryRepository $repository
+     * @param CategoryRepository $categoryRepository
+     * @return Response
+     */
     #[Route('', name: 'app_blog')]
-    public function index(Request $request, EntryRepository $repository): Response
+    #[Route('/category/{slug}', name: 'app_blog_category')]
+    public function index(
+        Request            $request,
+        EntryRepository    $repository,
+        CategoryRepository $categoryRepository,
+    ): Response
     {
-        $entries = $repository->findBy(['type' => 'blog'], ['id' => 'desc'], 6);
+        $slug = $request->get('slug');
+        $entries = $repository->findEntriesByCondition($slug, 'blog', 12);
 
         return $this->render('blog/index.html.twig', [
             'entries' => $entries,
+            'category' => $categoryRepository->findOneBy(['slug' => $slug]),
         ]);
     }
 
+    /**
+     * @param Request $request
+     * @param EntryRepository $repository
+     * @param TranslatorInterface $translator
+     * @return Response
+     */
     #[Route('/{slug}', name: 'app_blog_view')]
-    public function view(Request $request, EntryRepository $repository) {
-
+    public function view(
+        Request             $request,
+        EntryRepository     $repository,
+        TranslatorInterface $translator,
+    ): Response
+    {
         $entry = $repository->findOneBy(['slug' => $request->get('slug')]);
+
+        if (is_null($entry)) {
+            throw $this->createNotFoundException($translator->trans('http_error_404.description'));
+        }
 
         return $this->render('blog/view.html.twig', [
             'entry' => $entry,
--- src/Repository/EntryRepository.php
+++ src/Repository/EntryRepository.php
@@ -25,4 +25,28 @@ class EntryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Entry::class);
     }
+
+    /**
+     * @param string|null $slug
+     * @param string|null $type
+     * @param int $limit
+     * @return Entry[]|null
+     */
+    public function findEntriesByCondition(string $slug = null, string $type = null, int $limit = 12): ?array
+    {
+        $qb = $this->createQueryBuilder('e')
+            ->leftJoin('e.entryCategories', 'ec', 'WITH', 'ec.entry = e.id')
+            ->leftJoin('App\Entity\Category', 'c', 'WITH', 'c.id = ec.category');
+
+        if ($slug) {
+            $qb->where('c.slug = :slug')->setParameter('slug', $slug);
+        } else {
+            return $this->findBy(['type' => $type], ['id' => 'desc'], $limit);
+        }
+
+        if ($limit) {
+            $qb->orderBy('e.id',  'desc')->setMaxResults($limit);
+        }
+        return $qb->setCacheable(true)->getQuery()->getResult();
+    }
 }
