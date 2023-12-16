1d7a5e48273a0d4706e0ae711a6e9e5cf7ef728f
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2023-12-16 08:28:38 +0200

    improve blog design

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,13 +5,16 @@ namespace App\Controller;
 use App\Repository\CategoryRepository;
 use App\Repository\EntryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\{Request, Response};
+use Symfony\Component\HttpFoundation\{
+    Request,
+    Response
+};
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/blog')]
-class BlogController extends AbstractController
-{
+class BlogController extends AbstractController {
+
     /**
      * @param Request $request
      * @param EntryRepository $repository
@@ -20,26 +23,26 @@ class BlogController extends AbstractController
      */
     #[Route('', name: 'app_blog')]
     #[Route('/category/{slug}', name: 'app_blog_category')]
+    #[Route('/date/{date}', name: 'app_blog_date')]
     public function index(
-        Request            $request,
-        EntryRepository    $repository,
-        CategoryRepository $categoryRepository,
-    ): Response
-    {
+            Request $request,
+            EntryRepository $repository,
+            CategoryRepository $categoryRepository,
+    ): Response {
         $slug = $request->get('slug');
         $entries = $repository->findEntriesByCondition($slug, 'blog', 12);
         $category = $categoryRepository->findOneBy(['slug' => $slug]);
-        
+
         $categories = null;
-        
-        if(!$category) {
+
+        if (!$category) {
             $categories = $categoryRepository->findAll();
-        }        
+        }
 
         return $this->render('blog/index.html.twig', [
-            'entries' => $entries,
-            'category' => $category,
-            'categories' => $categories,
+                    'entries' => $entries,
+                    'category' => $category,
+                    'categories' => $categories,
         ]);
     }
 
@@ -51,11 +54,10 @@ class BlogController extends AbstractController
      */
     #[Route('/{slug}', name: 'app_blog_view')]
     public function view(
-        Request             $request,
-        EntryRepository     $repository,
-        TranslatorInterface $translator,
-    ): Response
-    {
+            Request $request,
+            EntryRepository $repository,
+            TranslatorInterface $translator,
+    ): Response {
         $entry = $repository->findOneBy(['slug' => $request->get('slug')]);
 
         if (is_null($entry)) {
@@ -63,7 +65,7 @@ class BlogController extends AbstractController
         }
 
         return $this->render('blog/view.html.twig', [
-            'entry' => $entry,
+                    'entry' => $entry,
         ]);
     }
 }
