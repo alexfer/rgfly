561f8e4935dd6bb4f18019c458681c4aa1ce6bc8
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2023-12-18 18:56:38 +0200

    impoert missing classes

--- src/Entity/MarketPlace/MarketProduct.php
+++ src/Entity/MarketPlace/MarketProduct.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: MarketProductRepository::class)]
 #[UniqueEntity(fields: ['slug'], message: 'slug.unique')]
