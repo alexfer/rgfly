5b5a44d74cca524c2410d4b6bb329c9d9bfaca9d
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2024-01-20 15:56:39 +0200

    display market info

--- src/Controller/MarketPlace/MarketController.php
+++ src/Controller/MarketPlace/MarketController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller\MarketPlace;
 
+use App\Entity\MarketPlace\Market;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{
+    Request, Response,
+};
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/market-place/market')]
 class MarketController extends AbstractController
 {
-    #[Route('', name: 'app_market_place_market')]
+    #[Route('', name: 'app_market_place_markets')]
     public function index(): Response
     {
         return $this->render('market_place/market/index.html.twig', []);
+    }
+
+    #[Route('/{slug}', name: 'app_market_place_market')]
+    public function market(Request $request, Market $market): Response
+    {
+        return $this->render('market_place/market/market.html.twig', [
+            'market' => $market,
+        ]);
     }
 }
