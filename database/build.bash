a158d6dbb04ceb3662058aa865ba75e40fb01d8b
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2024-02-10 09:53:56 +0200

    init new branch

--- database/build.bash
+++ database/build.bash
@@ -4,6 +4,6 @@ rm -rfv migrations/*
 
 php bin/console doctrine:database:drop --if-exists --force
 php bin/console doctrine:database:create
-php bin/console doctrine:migrations:migrate
+php bin/console make:migration --no-interaction
 php bin/console doctrine:migrations:migrate --no-interaction
 php bin/console doctrine:fixtures:load --no-interaction
