94230cd5148f1a165c65443821436ad159a673b7
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2024-02-18 08:19:17 +0200

    create cabinet sceleton & cleanup

--- src/Controller/Dashboard/IndexController.php
+++ src/Controller/Dashboard/IndexController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Dashboard;
 
 use App\Entity\MarketPlace\Market;
-use App\Entity\User;
 use App\Service\Dashboard;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerExceptionInterface;
@@ -12,8 +11,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\ExpressionLanguage\Expression;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/dashboard')]
 class IndexController extends AbstractController
@@ -34,9 +31,6 @@ class IndexController extends AbstractController
         EntityManagerInterface $em,
     ): Response
     {
-        if(in_array('ROLE_CUSTOMER', $user->getRoles(), true)) {
-            $this->createAccessDeniedException('Access Denied.');
-        }
         $markets = $em->getRepository(Market::class)->findBy($this->criteria($user, null, 'owner'));
         return $this->render('dashboard/content/index.html.twig', $this->navbar() + [
                 'markets' => $markets,
