d0232601b9ce7e1e1b4857e0d2cac1e48baaec39
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2024-03-27 20:12:24 +0200

    fixes twe

--- assets/tailwind/bootstrap.js
+++ assets/tailwind/bootstrap.js
@@ -1,8 +1,3 @@
-import 'flowbite';
 import {Dropdown, initTWE, Offcanvas, Ripple} from "tw-elements";
 
 initTWE({Offcanvas, Ripple, Dropdown});
-
-
-const modal = new Modal(document.getElementById('modal'), {});
-modal.hide();
