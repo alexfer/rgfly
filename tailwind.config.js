5e551575dbf02418308ef5de54d4c55c4b1f9e7c
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2024-04-19 06:41:55 +0300

    matket page UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
             gridTemplateColumns: {
                 'auto-fill-200': 'repeat(auto-fill, minmax(200px, 1fr))',
                 'auto-fit-200': 'repeat(auto-fit, minmax(200px, 1fr))',
+                'auto-fill-150': 'repeat(auto-fill, minmax(150px, 1fr))',
+                'auto-fit-150': 'repeat(auto-fit, minmax(150px, 1fr))',
             },
         },
     },
