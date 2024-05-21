0c6f31d443ccb6db7bb0eff355e8efc57782d1d1
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2024-05-21 09:02:55 +0300

    cleanup

--- src/Repository/MarketPlace/MarketMessageRepository.php
+++ src/Repository/MarketPlace/MarketMessageRepository.php
@@ -11,33 +11,12 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class MarketMessageRepository extends ServiceEntityRepository
 {
+    /**
+     * @param ManagerRegistry $registry
+     */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, MarketMessage::class);
     }
 
-    //    /**
-    //     * @return MarketMessage[] Returns an array of MarketMessage objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('m.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?MarketMessage
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
