788951163654af9f9a005a4ad7b9d60a75dad535
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2024-06-04 10:18:19 +0300

    get raw query for ptoduct

--- src/Controller/MarketPlace/ProductController.php
+++ src/Controller/MarketPlace/ProductController.php
@@ -2,22 +2,27 @@
 
 namespace App\Controller\MarketPlace;
 
-use App\Entity\MarketPlace\StoreCustomer;
-use App\Entity\MarketPlace\StoreProduct;
+use App\Entity\MarketPlace\{StoreCustomer, StoreProduct};
+use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{Request, Response};
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[Route('/market-place/product')]
 class ProductController extends AbstractController
 {
+    /**
+     * @param Request $request
+     * @param UserInterface|null $user
+     * @param EntityManagerInterface $em
+     * @return Response
+     * @throws Exception
+     */
     #[Route('/{slug}/{tab}', name: 'app_market_place_product')]
     public function index(
         Request                $request,
-        StoreProduct           $product,
         ?UserInterface         $user,
         EntityManagerInterface $em,
     ): Response
@@ -26,8 +31,10 @@ class ProductController extends AbstractController
             'member' => $user,
         ]);
 
+        $product = $em->getRepository(StoreProduct::class)->fetchProduct($request->get('slug'));
+
         return $this->render('market_place/product/index.html.twig', [
-            'product' => $product,
+            'product' => $product['product'],
             'customer' => $customer,
         ]);
     }
