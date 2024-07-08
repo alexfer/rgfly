2d672d6d7f9514110badfdf6bd251477f7b1dd9a
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2024-07-08 08:02:35 +0300

    deploy elasticsearch

--- src/Controller/MarketPlace/SearchController.php
+++ src/Controller/MarketPlace/SearchController.php
@@ -4,11 +4,25 @@ namespace App\Controller\MarketPlace;
 
 use App\Entity\MarketPlace\StoreCategory;
 use Doctrine\ORM\EntityManagerInterface;
+use Elastic\Elasticsearch\ClientBuilder;
+use Elastic\Elasticsearch\Exception\AuthenticationException;
+use Elastic\Elasticsearch\Exception\ClientResponseException;
+use Elastic\Elasticsearch\Exception\ServerResponseException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/market-place')]
 class SearchController extends AbstractController
 {
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->dsn = $params->get('app.elastic.dsn');
+    }
+
     /**
      * @param EntityManagerInterface $em
      * @return Response
@@ -19,5 +33,26 @@ class SearchController extends AbstractController
         return $this->render('market_place/search/categories.html.twig', [
             'categories' => $categories,
         ]);
+    }
+
+    /**
+     * @throws AuthenticationException
+     * @throws ClientResponseException
+     * @throws ServerResponseException
+     */
+    #[Route('/search', name: 'app_market_place_search')]
+    public function find(
+        Request       $request
+    ): Response
+    {
+        $client = ClientBuilder::create()->setHosts([$this->dsn])->build();
+        try {
+            $response = $client->getTransport();
+        } catch (ClientResponseException|ServerResponseException $e) {
+            throw new ServerResponseException($e->getMessage());
+        }
+
+
+        dd($response);
     }
 }
