bea909411c0c6ddc53b63413f6d5be6c46d8b5ae
Aleksander Sh <aleksandersh@MacBook-Air-Aleksander.local>
2024-07-14 11:21:29 +0300

    remove docker

--- docker/init/psql.sql
+++
@@ -1,8 +0,0 @@
--- Change "<database>" name to your database
-DROP ROLE IF EXISTS rgfly;
-CREATE USER rgfly WITH password 'rgfly';
-ALTER USER rgfly WITH SUPERUSER;
-ALTER USER rgfly CREATEDB;
-CREATE DATABASE rgfly OWNER rgfly;
-GRANT ALL PRIVILEGES ON DATABASE rgfly TO rgfly;
-ALTER DATABASE rgfly SET timezone TO 'Europe/Kyiv';
